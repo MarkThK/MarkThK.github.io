32dac7b6dd5821ea268044a2280337abbeec270c
MarkThK <m55a88r@gmail.com>
2026-05-31 18:20:25 +0300

    Update posts.js

--- data-science/posts.js
+++ data-science/posts.js
@@ -10,5 +10,11 @@ const posts = [
     date: "May 2026",
     description: "My first Project in Computer Vision. (part 2)",
     link: "post2.html"
+  },
+  {
+    title: "Analyzing the Diversity of Greek Online News Articles",
+    date: "May 2026",
+    description: "News diversity over time; using data from the Internet in DS projects. (Part 1)",
+    link: "post3.html"
   }
 ];
